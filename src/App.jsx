5a5c94f89c93f6c576f607e2c2879f9ce1fbe52c
Pete Hamrick <pete.hamrick@gmail.com>
2022-01-10 13:31:42 -0800

    wrote basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,35 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {/* Dashboard */}
+          {/* Conditionally render */}
+          {/* if not signed in, welcome! links to sign in/signup */}
+          {/* if signed in shows all your dashboard */}
+        </Route>
+
+        <Route path="/login">
+          {/* Auth */}
+          {/* redirect to dashboard */}
+        </Route>
+
+        <Route path="/register">
+          {/* Auth */}
+          {/* redirect to dashboard */}
+        </Route>
+
+        <Route path="/about">{/* AboutUs */}</Route>
+
+        {/* Private Routes Below */}
+        <Route path="/notes">
+          {/* Notes container, view, edit, etc based on state */}
+        </Route>
+
+        {/* may need more routes? */}
+      </Switch>
+    </Router>
+  );
 }
