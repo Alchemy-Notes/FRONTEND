1ae56740b70159b6b9469e92aefe40173309fa3f
Alice-Retz <alicegretz@gmail.com>
2022-01-17 15:24:38 -0600

    still trying to figure out dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,52 @@ import Dashboard from './views/Dashboard/Dashboard';
 import Notes from './views/Notes/Notes';
 import styles from './App.css';
 import Hamburger from './components/Hamburger/Hamburger';
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function App() {
-  const [darkMode, setDarkMode] = React.useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+
+  // useEffect(() => {
+  //   const json = localStorage.getItem('noted-dark-mode');
+  //   const currentMode = JSON.parse(json);
+  //   if (currentMode) {
+  //     setDarkMode(true);
+  //   } else {
+  //     setDarkMode(false);
+  //   }
+  // }, []);
+
+  // useEffect(() => {
+  //   if (darkMode) {
+  //     document.body.classList.add('dark');
+  //   } else {
+  //     document.body.classList.remove('dark');
+  //   }
+  //   const json = JSON.stringify(darkMode);
+  //   localStorage.setItem('noted-dark-mode', json);
+  // }, [darkMode]);
 
   useEffect(() => {
-    const json = localStorage.getItem('noted-dark-mode');
-    const currentMode = JSON.parse(json);
-    if (currentMode) {
-      setDarkMode(true);
-    } else {
-      setDarkMode(false);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (darkMode) {
-      document.body.classList.add('dark');
-    } else {
-      document.body.classList.remove('dark');
-    }
-    const json = JSON.stringify(darkMode);
-    localStorage.setItem('noted-dark-mode', json);
+    document.body.classList.toggle('dark', darkMode);
   }, [darkMode]);
+
+  const handleClick = () => {
+    if (darkMode) {
+      setDarkMode(false);
+    } else {
+      setDarkMode(true);
+    }
+  };
 
   return (
     <Router>
       <main className={styles.container}>
         <Hamburger />
-        <button onClick={() => setDarkMode(!darkMode)}>Dark Mode</button>
+        {/* <button onClick={() => setDarkMode(darkMode)}>Dark Mode</button> */}
+        {/* <button onClick={handleClick} className={darkMode ? styles.dark : null}> */}
+        <button onClick={handleClick} className={darkMode ? styles.dark : null}>
+          Dark Mode
+        </button>
         <Switch>
           <Route exact path="/">
             <Auth />
