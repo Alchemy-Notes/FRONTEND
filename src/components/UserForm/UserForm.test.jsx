f743ba75ad501fb707eaf7d73fadcc62c1d228be
Alice-Retz <alicegretz@gmail.com>
2022-01-18 17:09:39 -0600

    Test snapshots updated

--- src/components/UserForm/UserForm.test.jsx
+++ src/components/UserForm/UserForm.test.jsx
@@ -1,11 +1,14 @@
 import { render } from '@testing-library/react';
+import { ThemeProvider } from '../../context/ThemeContext';
 import { UserProvider } from '../../context/UserContext';
 import UserForm from './UserForm';
 
 it('should display an empty user form for signing up/in', () => {
   const { container } = render(
     <UserProvider>
-      <UserForm />
+      <ThemeProvider>
+        <UserForm />
+      </ThemeProvider>
     </UserProvider>
   );
 
