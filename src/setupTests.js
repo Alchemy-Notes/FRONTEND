5e982d83711fe9464c25a59bef670c8f661d4174
Pete Hamrick <pete.hamrick@gmail.com>
2022-01-18 14:39:12 -0800

    fixed add tags test failing due to react-markdown, mock react-markdown in setupTest.js and this makes the tests pass, mobbed with alice paul and triana

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,2 +1,6 @@
 global.fetch = (...args) =>
   import('cross-fetch').then(({ default: fetch }) => fetch(...args));
+
+jest.mock('react-markdown', () => (props) => {
+  return <>{props.children}</>;
+});
