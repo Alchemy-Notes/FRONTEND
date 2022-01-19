2329dd5de2bb248666712325e7e3dac316ad59b2
Alice-Retz <alicegretz@gmail.com>
2022-01-19 17:50:48 -0600

    About us and Home styled, Home view created.

--- src/components/Hamburger/Hamburger.jsx
+++ src/components/Hamburger/Hamburger.jsx
@@ -57,58 +57,58 @@ export default class Hamburger extends Component {
       },
     };
 
-    // const darkMenu = {
-    //   bmBurgerButton: {
-    //     position: 'fixed',
-    //     width: '36px',
-    //     height: '30px',
-    //     left: '36px',
-    //     top: '36px',
-    //   },
-    //   bmBurgerBars: {
-    //     background: '#ffffff',
-    //   },
-    //   bmBurgerBarsHover: {
-    //     background: '#a90000',
-    //   },
-    //   bmCrossButton: {
-    //     height: '24px',
-    //     width: '24px',
-    //   },
-    //   bmCross: {
-    //     background: '#bdc3c7',
-    //   },
-    //   bmMenuWrap: {
-    //     position: 'fixed',
-    //     height: 'auto',
-    //   },
-    //   bmMenu: {
-    //     background: '#FFDDD6',
-    //     border: '#FFB1A0 solid 3px',
-    //     borderRadius: '20px',
-    //     padding: '2px',
-    //     fontSize: '1.15em',
-    //   },
-    //   bmMorphShape: {
-    //     fill: '#373a47',
-    //   },
-    //   bmItemList: {
-    //     color: '#b8b7ad',
-    //     padding: '0.8em',
-    //   },
-    //   bmItem: {
-    //     display: 'inline-block',
-    //   },
-    //   bmOverlay: {
-    //     background: 'rgba(0, 0, 0, 0.3)',
-    //   },
-    // };
+    const darkMenu = {
+      bmBurgerButton: {
+        position: 'relative',
+        width: '36px',
+        height: '30px',
+        left: '36px',
+        top: '10px',
+      },
+      bmBurgerBars: {
+        background: '#FBF5D6',
+      },
+      bmBurgerBarsHover: {
+        background: '#a90000',
+      },
+      bmCrossButton: {
+        height: '24px',
+        width: '24px',
+      },
+      bmCross: {
+        background: '#000',
+      },
+      bmMenuWrap: {
+        top: '69px',
+        position: 'fixed',
+        height: 'auto',
+      },
+      bmMenu: {
+        background: '#E9BDB3',
+        border: '#A56556 solid 3px',
+        borderRadius: '20px',
+        padding: '2px',
+        fontSize: '1.15em',
+      },
+      bmMorphShape: {
+        fill: '#373a47',
+      },
+      bmItemList: {
+        color: '#A56556',
+        padding: '0.8em',
+      },
+      bmItem: {
+        display: 'inline-block',
+      },
+      bmOverlay: {
+        background: 'rgba(0, 0, 0, 0)',
+      },
+    };
 
     return (
       <header>
         <nav>
-          {/* <Menu styles={theme ? lightMenu : darkMenu} noOverlay> */}
-          <Menu styles={lightMenu}>
+          <Menu styles={this.props.theme ? darkMenu : lightMenu}>
             <ul>
               <li>
                 <Link className="menu-item" to="/">
